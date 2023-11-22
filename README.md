7c1ef143966af6c44679d08dfd9e70c5d0cff0bb
HaoranZXD <haoranz@uw.edu>
2023-11-21 18:46:44 -0800

    tests uploaded

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TicTacToe-with-OOP
 A TicTacToe game with Object Oriented Programming feature
+
+## Running the Tests
+
+To run the unit tests for the Tic Tac Toe game, use the following command:
+
+```bash
+python -m unittest tests.py
+
---
+++ tests.py
@@ -0,0 +1,57 @@
+import unittest
+import io
+from unittest.mock import patch
+from game import TicTacToeGame
+from player import HumanPlayer, BotPlayer
+
+class TestTicTacToeGame(unittest.TestCase):
+
+    def test_initial_empty_board(self):
+        game = TicTacToeGame(HumanPlayer('X'), HumanPlayer('O'))
+        self.assertTrue(all(all(cell is None for cell in row) for row in game.board), "Board should be initialized empty")
+
+    def test_two_players_initialization(self):
+        player1 = HumanPlayer('X')
+        player2 = HumanPlayer('O')
+        game = TicTacToeGame(player1, player2)
+        self.assertEqual(game.players[0].symbol, 'X')
+        self.assertEqual(game.players[1].symbol, 'O')
+
+    def test_single_player_initialization(self):
+        player = HumanPlayer('X')
+        bot = BotPlayer('O')
+        game = TicTacToeGame(player, bot)
+        self.assertIsInstance(game.players[0], HumanPlayer)
+        self.assertIsInstance(game.players[1], BotPlayer)
+
+    def test_alternating_turns(self):
+        game = TicTacToeGame(HumanPlayer('X'), HumanPlayer('O'))
+        self.assertEqual(game.current_player, 0)
+        game.board[0][0] = 'X'
+        game.current_player = 1 - game.current_player
+        self.assertEqual(game.current_player, 1)
+
+    def test_detect_win(self):
+        game = TicTacToeGame(HumanPlayer('X'), HumanPlayer('O'))
+        game.board = [['X', 'X', 'X'], [None, 'O', None], [None, None, 'O']]
+        self.assertEqual(game.get_winner(), 'X')
+
+    def test_detect_draw(self):
+        game = TicTacToeGame(HumanPlayer('X'), HumanPlayer('O'))
+        game.board = [['X', 'O', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']]
+        self.assertEqual(game.get_winner(), 'Draw')
+
+    def test_viable_spots_only(self):
+        with patch('builtins.input', side_effect=['0,0', '1,1']), patch('sys.stdout', new=io.StringIO()):
+            game = TicTacToeGame(HumanPlayer('X'), HumanPlayer('O'))
+            game.board[0][0] = 'X'  
+            row, col = game.players[0].make_move(game.board)
+            self.assertNotEqual((row, col), (0, 0))  # Assert the move is not on the taken spot
+
+    def test_correct_winner(self):
+        game = TicTacToeGame(HumanPlayer('X'), HumanPlayer('O'))
+        game.board = [['X', None, 'O'], ['O', 'X', 'O'], ['X', None, 'X']]
+        self.assertEqual(game.get_winner(), 'X')
+
+if __name__ == '__main__':
+    unittest.main()
